7aea87c9fc4afc71df5e8b470eeffdb31fe962d1
mamaker <madhuvasudevan@hotmail.com>
2015-05-20 00:47:48 -0500

    Done!

--- UpcastAnEntireList/Program.cs
+++ UpcastAnEntireList/Program.cs
@@ -10,6 +10,36 @@ namespace UpcastAnEntireList
     {
         static void Main(string[] args)
         {
+            List<Duck> ducks = new List<Duck>() {
+                new Duck() { Kind = KindOfDuck.Mallard, Size = 17 },
+                new Duck() { Kind = KindOfDuck.Muscovy, Size = 18 },
+                new Duck() { Kind = KindOfDuck.Decoy, Size = 14 },
+                new Duck() { Kind = KindOfDuck.Muscovy, Size = 11 },
+                new Duck() { Kind = KindOfDuck.Mallard, Size = 14 },
+                new Duck() { Kind = KindOfDuck.Decoy, Size = 13 },
+            };
+            IEnumerator<Duck> enumerator = ducks.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Duck duck = enumerator.Current;
+                Console.WriteLine(duck);
+            }
+            IDisposable disposable = enumerator as IDisposable;
+            if (disposable != null) disposable.Dispose();
+            
+            IEnumerable<Bird> upcastDucks = ducks;
+
+            List<Bird> birds = new List<Bird>();
+            birds.Add(new Bird() { Name = "Feathers" });
+            birds.AddRange(ducks);
+            birds.Add(new Penguin() { Name = "George" });
+
+            foreach (Bird bird in birds)
+            {
+                Console.WriteLine(bird);
+            }
+
+
         }
     }
 }
